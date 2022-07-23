88b91bb2ec1c760e383944d15177f747b76e6bb1
Viktoriya <anviktoriya@yandex.ru>
2022-07-23 15:59:55 +0300

    Add prTsk64

---
+++ Task64/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Homework9
+{
+    class Task64
+    {
+        // // Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+        //  в промежутке от M до N.
+        // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+        // M = 4; N = 8. -> ""4, 6, 7, 8""
+        public static void Main()
+        {
+            Console.WriteLine($"ВВедите число М");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"ВВедите число N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            if (PrintNumber(m, n ) < 0)
+            {
+                Console.WriteLine($"N должно быть больше M");
+            }
+            else
+            {
+                Console.WriteLine($"Ряд натуральных чисел:");
+                PrintNumber(m, n);
+            }
+        }
+        public static int PrintNumber(int m, int n)
+        {
+            if (m == n && m != 0)
+            {
+                Console.WriteLine("" + m);
+                return m;
+            }
+            else
+                if (m < n)
+            {
+                Console.WriteLine("" + m);
+                return PrintNumber(m + 1, n);
+            }
+            else
+            {
+                return n - m;
+            }
+        }
+    }
+}
+
